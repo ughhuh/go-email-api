ebabc88eaa4f9502c7ffedd57fb3aa48d20a2deb
Alena Galysheva <alena.galysheva@wapice.com>
2024-10-18 13:54:17 +0300

    basic api points done

--- cmd/app/emails.go
+++ cmd/app/emails.go
@@ -1,21 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"math/rand"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 type ArrayResponse struct {
 	Data []Email `json:"data"`
 }
 
+type User struct {
+	Address string `json:"email_address"`
+}
+
 type Email struct {
-	MessageId string
-	Body      string
-	From      []string
-	To        []string
+	MessageId string   `json:"message_id"`
+	Body      string   `json:"body"`
+	From      []string `json:"from"`
+	To        []string `json:"to"`
+}
+
+type CreateEmailRequest struct {
+	Username string `json:"username"`
 }
 
 func getEmailsForUser(c *gin.Context) {
@@ -58,4 +72,79 @@ func getEmailById(c *gin.Context) {
 		}
 	}
 	c.JSON(200, email)
+}
+
+func createNewInbox(c *gin.Context) {
+	// perhaps i should allow to custom make
+	var requestBody CreateEmailRequest
+	json.NewDecoder(c.Request.Body).Decode(&requestBody)
+	// so we generate a new email inbox
+	if requestBody.Username == "" {
+		requestBody.Username = uuid.NewString()
+	}
+	allowedDomains := viper.GetStringSlice("allowed_domains")
+	domainIndex := rand.Intn(len(allowedDomains))
+	username := requestBody.Username + "@" + allowedDomains[domainIndex]
+	// post new inbox
+	db := connectToDb()
+	query := `INSERT INTO users("email_address") values ($1);`
+	_, err := db.Exec(query, username)
+	db.Close()
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(500, "Failed to create email address")
+		return
+	}
+	// send new inbox to user
+	c.JSON(201, User{Address: username})
+}
+
+func deleteInbox(c *gin.Context) {
+	// get email address from body
+	var requestBody User
+	json.NewDecoder(c.Request.Body).Decode(&requestBody)
+	if requestBody.Address == "" {
+		c.JSON(400, "Invalid email")
+		return
+	}
+	// get emails
+	db := connectToDb()
+	query := `select mail_id from inboxes where user_id = $1`
+	rows, err := db.Query(query, requestBody.Address)
+	db.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	emails := []string{}
+	for rows.Next() {
+		var entry string
+		err = rows.Scan(&entry)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		emails = append(emails, entry)
+	}
+	// delete user and inboxes
+	db = connectToDb()
+	query = `DELETE FROM users WHERE email_address = $1;`
+	_, err = db.Exec(query, requestBody.Address)
+	db.Close()
+	// return on success
+	if err != nil {
+		log.Fatal(err)
+		c.JSON(500, "Failed to delete email address")
+		return
+	}
+	// delete email entries
+	db = connectToDb()
+	emails_str := fmt.Sprint(strings.Join(emails, ","))
+	query = `delete from emails where message_id in ($1)`
+	_, err = db.Exec(query, emails_str)
+	db.Close()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	c.JSON(200, "all good")
 }
--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func main() {
 	// setup
+	configLoader("config.json") // load config
 	router := gin.Default()
 	// get emails for email inbox
 	router.GET("/inbox/:email_id", getEmailsForUser)
 	router.GET("/email/:email_id", getEmailById)
 
-	router.Run(":8080")
+	router.POST("/email", createNewInbox)
+	router.DELETE("/email", deleteInbox)
+
+	port := ":" + viper.GetString("PORT")
+	router.Run(port)
+}
+
+func configLoader(configName string) {
+	viper.SetConfigName(configName)
+	viper.SetConfigType(strings.Split(configName, ".")[1])
+	viper.AddConfigPath(".")     // look in current dir
+	viper.AddConfigPath("../..") // look in parent dir
+	err := viper.ReadInConfig()  // red the config file
+	if err != nil {
+		panic(fmt.Errorf("error when reading config file: %s", err))
+	}
+	// watch config for changes
+	viper.WatchConfig()
 }
